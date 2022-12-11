115ae91bd49e4df35840d939d220ffe835d0cb45
Christian Traintinger <c.traintinger@gmx.net>
2022-12-11 17:14:49 +0100

    End of chapter 13

--- src/main/java/com/polarbookshop/edgeservice/user/UserController.java
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.edgeservice.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,11 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class UserController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
 	@GetMapping("user")
 	public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+		log.info("Fetching information about the currently authenticated user");
 		User user = new User(
 			oidcUser.getPreferredUsername(),
 			oidcUser.getGivenName(),
